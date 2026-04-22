b984c812bc509e9fa469f3be08b0b21c83de55a5
Kyle S. <22063898+KSherman97@users.noreply.github.com>
2026-04-21 22:03:59 -0400

    Add: find-target-in-rotated-sorted-array - submission-3

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-3.py
@@ -0,0 +1,34 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        # we will need to use binary search
+        # split in half by comparing the pivot to the mid point
+        # repeat until the left and right pointer are the same position and this is our solution
+
+        left, right = 0, len(nums) - 1
+
+
+        while left <= right:
+            mid_point = (left+right) // 2
+
+            if target == nums[mid_point]:
+                return mid_point
+
+
+            # if left < mid the pivot must be in the right
+            # this is case number one where the left half is sorted
+            if nums[left] <= nums[mid_point]:
+                if nums[left] <= target < nums[mid_point]:
+                    right = mid_point - 1
+                else:
+                    left = mid_point + 1
+                
+            # if the mid < right then the pivot must be in the left
+            # this is case number two wher the right half is sorted
+            else:
+                if nums[mid_point] < target <= nums[right]:
+                    left = mid_point+1
+                else:
+                    right = mid_point - 1
+
+        return -1
+        
