d9427f4c426c1b9f926b8d9bbe3f69c8709bbfb7
Kyle S. <22063898+KSherman97@users.noreply.github.com>
2026-04-21 20:09:32 -0400

    Add: validate-parentheses - submission-29

---
+++ Data Structures & Algorithms/validate-parentheses/submission-29.py
@@ -0,0 +1,25 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+
+        # we can optimize this solution by utilizing a hasmap to compare characters with their closing pair
+        stack = []
+        char_map = {"}":"{", ")":"(", "]":"["}
+
+        if len(s) % 2 == 1:
+            return False
+        
+        for char in s:
+            if char in char_map:
+                top_char = ""
+                if len(stack) > 0:
+                    top_char = stack.pop()
+
+                if char_map[char] != top_char:
+                    return False
+
+            else:
+                stack.append(char)
+        
+        if len(stack) == 0:
+            return True
+        return False
